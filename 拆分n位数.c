7101dfddd198d22f2a54322a28451c8e54a260ac
xiao <2740650534@qq.com>
2024-03-15 22:41:45 +0800

    拆分n位数

---
+++ 拆分n位数.c
@@ -0,0 +1,28 @@
+//拆一个n位数
+#include<stdio.h>
+int main()
+{
+int num=0;
+printf("请输入要拆分的数。\n");
+scanf("%d",&num);
+while(1)
+{
+    if(num/10==0)   //先判断是个位数还是多位数
+    {
+    printf("%d\n",num); 
+    break;
+    }
+    else
+    {
+    printf("%d\n",num%10);
+    num=num/10;
+    }
+}
+
+
+
+
+
+    return 0;
+
+}
